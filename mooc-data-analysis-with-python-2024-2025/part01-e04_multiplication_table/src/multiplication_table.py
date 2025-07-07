0a43451a9f339371fc522d33f6fc9179aa097c4a
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-07 17:30:51 +0100

    Exercie 1.4 test completed

--- mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,14 @@
 
 
 def main():
-    pass
+    i=1
+    while i <= 10:
+        j=1
+        while j <=10:
+            print(f"{i*j:4}", end="")
+            j+=1
+        print()
+        i+=1
 
 if __name__ == "__main__":
     main()
