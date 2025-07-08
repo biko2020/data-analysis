24fdc3f1b7d02ce917d17c274fc9988b3181979f
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-08 10:56:27 +0100

    Exercise 1.6 submited

--- mooc-data-analysis-with-python-2024-2025/part01-e06_triple_square/src/triple_square.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+def triple(i):
+    return i * 3
+
+def square(i):
+    return i ** 2
 
 def main():
-    pass
-
+    for j in range(1, 11):
+        squ = square(j)
+        trip = triple(j)
+        if squ > trip:
+            break
+        print(f"triple({j})=={trip} square({j})=={squ}")
 if __name__ == "__main__":
     main()
