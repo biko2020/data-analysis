1d6db0e2223a33abf79d2058bba6f8b2933bd95c
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-09 15:41:38 +0100

    Exercise 01 10 submited

--- mooc-data-analysis-with-python-2024-2025/part01-e10_detect_ranges/src/detect_ranges.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    return []
+    L = sorted(L)
+    result = []
+    i= 0
+    while i < len(L):
+        start =L[i]
+        end = start +1 
+        while i + 1 < len(L) and L[i + 1] == L[i] +1:
+            i+=1
+            end = L[i]+1
+        if end > start +1:
+            result.append((start, end))
+        else:
+            result.append(start)
+        i+=1
+    return result
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
