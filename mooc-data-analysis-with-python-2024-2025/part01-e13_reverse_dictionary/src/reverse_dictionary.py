c18a8be073f01b32f6fc9cae828db0ec73dcdb5e
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-13 08:52:55 +0100

    Exercise 01.13 submited

--- mooc-data-analysis-with-python-2024-2025/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    reversed_dict = {}
+    for english_word, finnish_list in d.items():
+        for finnish_word in finnish_list:
+            if finnish_word not in reversed_dict:
+                reversed_dict[finnish_word]=[]
+            reversed_dict[finnish_word].append(english_word)
+    return reversed_dict
 
 def main():
-    pass
+    d={
+       'move': ['liikuttaa'], 
+       'hide': ['piilottaa', 'salata'], 
+       'six': ['kuusi'], 
+       'fir': ['kuusi']
+       }
+    reversed_d = reverse_dictionary(d)
+    print(reversed_d) 
 
 if __name__ == "__main__":
     main()
