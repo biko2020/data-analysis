c973faa738bab1340e22112b5476711dc24b97e7
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-13 09:30:42 +0100

    Exercise 1.14 sumited

--- mooc-data-analysis-with-python-2024-2025/part01-e14_find_matching/src/find_matching.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    positions =[]
+    for index, word in enumerate(L):
+        if pattern in word:
+            positions.append(index)
+
+    return positions
 
 def main():
-    pass
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
 
 if __name__ == "__main__":
     main()
