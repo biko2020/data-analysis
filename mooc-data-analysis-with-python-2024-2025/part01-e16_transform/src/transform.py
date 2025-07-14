e8dccdacfad37040d406cd946b285fcef24240d7
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-14 11:49:51 +0100

    Exercise 01.16 submited

--- mooc-data-analysis-with-python-2024-2025/part01-e16_transform/src/transform.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e16_transform/src/transform.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    L1 = list(map(int, s1.split()))
+    L2 = list(map(int, s2.split()))
+    return list(map(lambda L: L[0]*L[1], zip(L1,L2)))
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1"))
 
 if __name__ == "__main__":
     main()
