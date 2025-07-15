acc077b3ff6170d1c6e79366daf80e02a9979fde
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-15 09:14:31 +0100

    Exercises 01.17 18 19 submited

--- mooc-data-analysis-with-python-2024-2025/part01-e17_positive_list/src/positive_list.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+
+    return (list(filter(lambda number:number > 0, L)))
 
 def main():
-    pass
+    L = [2,-2,0,1,-7]
+    print(positive_list(L))
 
 if __name__ == "__main__":
     main()
--- mooc-data-analysis-with-python-2024-2025/part01-e19_sum_equation/src/sum_equation.py
+++ mooc-data-analysis-with-python-2024-2025/part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    if not L:
+        return "0 = 0"
+    sumofelements = sum(L)
+    reslut = " + ".join(str(x) for x in L) + " = " + str(sumofelements)
+    return reslut
 
 def main():
-    pass
+    print(sum_equation([1,5,7]))
 
 if __name__ == "__main__":
     main()
