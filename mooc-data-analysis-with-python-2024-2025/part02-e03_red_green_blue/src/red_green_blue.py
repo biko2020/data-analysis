50609119a285e4fca4cd52e6e4010c1f8342ec6f
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-22 15:16:50 +0100

    Exercise 02.02 submited

--- mooc-data-analysis-with-python-2024-2025/part02-e03_red_green_blue/src/red_green_blue.py
+++ mooc-data-analysis-with-python-2024-2025/part02-e03_red_green_blue/src/red_green_blue.py
@@ -3,11 +3,22 @@
 import re
 
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    result = []
+    with open(filename) as file:
+        next(file)
+        for line in file:
+            match = re.match(r'\s*(\d+)\s+(\d+)\s+(\d+)\s+(.*\S)', line)
+            if match:
+                red, green, blue, color = match.groups()
+                result.append(f"{red}\t{green}\t{blue}\t{color}")
+    return result
+
 
 
 def main():
-    pass
+    read_list = red_green_blue()
+    for line in read_list[:5]:
+        print(line)
 
 if __name__ == "__main__":
     main()
