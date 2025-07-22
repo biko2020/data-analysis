3af18fa1a376827eb8d5725f27e90e4b3222bed5
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-22 15:17:05 +0100

    Exercise 02.02 submited

--- mooc-data-analysis-with-python-2024-2025/part02-e02_file_listing/src/file_listing.py
+++ mooc-data-analysis-with-python-2024-2025/part02-e02_file_listing/src/file_listing.py
@@ -4,10 +4,25 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
-
+    result = []
+    pattern = re.compile(
+        r'^\S+\s+\d+\s+\S+\s+\S+\s+(\d+)\s+(\w{3})\s+(\d+)\s+(\d+):(\d+)\s+(.*)$'
+        )
+    with open(filename) as file:
+        for line in file:
+            mo = pattern.match(line)
+            if mo:
+                size =int(mo.group(1))
+                month = mo.group(2)
+                day = int(mo.group(3))
+                hour = int(mo.group(4))
+                minute = int(mo.group(5))
+                file_name = mo.group(6)
+                result.append((size,month,day,hour, minute, file_name))
+    return result
 def main():
-    pass
+    for item in file_listing():
+        print(item)
 
 if __name__ == "__main__":
     main()
