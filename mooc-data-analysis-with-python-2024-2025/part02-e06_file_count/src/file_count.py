2e7955443816fd1057411643c2f985214dc1ddb3
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-25 16:36:30 +0100

    Exercise file count

--- mooc-data-analysis-with-python-2024-2025/part02-e06_file_count/src/file_count.py
+++ mooc-data-analysis-with-python-2024-2025/part02-e06_file_count/src/file_count.py
@@ -3,10 +3,21 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    number_of_line =0
+    number_of_word =0
+    number_of_character= 0
+    with open(filename, 'r') as file:
+        for line in file:
+            number_of_line +=1
+            words = line.split()
+            number_of_word += len(words)
+            number_of_character += len(line)
+    return (number_of_line, number_of_word, number_of_character)
 
 def main():
-    pass
+    for filename in sys.argv[1:]:
+        lines, words, chars = file_count(filename)
+        print(f"{lines}\t{words}\t{chars}\t{filename}")
 
 if __name__ == "__main__":
     main()
