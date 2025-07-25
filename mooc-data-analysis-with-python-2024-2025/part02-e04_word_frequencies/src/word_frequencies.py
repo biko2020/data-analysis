84cd26678fabb67eac195e18382a4dd9c29f011c
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-25 15:01:48 +0100

    Excerces Word frequencies

--- mooc-data-analysis-with-python-2024-2025/part02-e04_word_frequencies/src/word_frequencies.py
+++ mooc-data-analysis-with-python-2024-2025/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+
 def word_frequencies(filename):
-    return {}
+    frequencies = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            words = line.split()
+            for word in words:
+                cleaned = word.strip("""!"#$%&'()*,-./:;?@[]_""").lower()
+                if cleaned:
+                    frequencies[cleaned] = frequencies.get(cleaned, 0) + 1
+    return frequencies
 
 def main():
-    pass
+    frequencies = word_frequencies("src/alice.txt")
+    for word, value in sorted(frequencies.items()):
+        print(f"{word}\t{value}")
 
 if __name__ == "__main__":
     main()
