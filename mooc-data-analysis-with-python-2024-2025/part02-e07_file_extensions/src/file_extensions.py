718895c4f6ca01f91dcd3574e8eb05fd73b6feb2
biko2020 <aitoufkirbrahimab@gmail.com>
2025-07-26 10:34:33 +0100

    file extensions

--- mooc-data-analysis-with-python-2024-2025/part02-e07_file_extensions/src/file_extensions.py
+++ mooc-data-analysis-with-python-2024-2025/part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 def file_extensions(filename):
-    return ([], {})
+    without_extension = []
+    with_extension = {}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            file_name = line.strip()  
+            if '.' not in file_name:
+                without_extension.append(file_name.lstrip('.'))
+            else:
+                parts = file_name.split('.')
+                extension= parts[-1]
+                with_extension.setdefault(extension,[]).append(file_name)
+
+    return (without_extension, with_extension)
 
 def main():
-    pass
+    without_ext, with_ext = file_extensions("src/filenames.txt")
+    print(f"{len(without_ext)} files with no extension")
+    for extension in sorted(with_ext):
+        print(f"{extension} {len(with_ext[extension])}")
 
 if __name__ == "__main__":
     main()
